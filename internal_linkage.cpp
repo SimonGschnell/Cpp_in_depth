095528f14647e3181793e67762f54f28307e2853
Simon Gschnell <simongschnell@gmail.com>
2023-10-14 21:55:18 +0200

    internal linkage for functions and variable identifiers

--- internal_linkage.cpp
+++ internal_linkage.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 #include <string>
 
+//! In modern C++, use of the static keyword for giving identifiers internal linkage is falling out of favor.
+//! Unnamed namespaces can give internal linkage to a wider range of identifiers
+
+
 // An identifier with 
 //? internal linkage 
 // can be seen and used within a file but is not accessible form other files , 
 // identifcally named identifiers in different files will be treated as independent
+//! this avoid naming collisions 
 
 //? const and constexpr global variables have internal linkage as default 
 const float gravity {9.81f};
@@ -16,9 +21,15 @@ int global_number {55};
 //! we can change the external linkage of non const global variables to internal
 static std::string global_string {"hello world"};
 
-int main (){
-
-
-
-    return 0;
+//?--------------------------------
+//* function identifiers have the same linkage property that variable identifiers do --> external
+// this function may cause naming collisions
+int add(int x, int y){ //? this can be forewarded
+    return x + y;
 }
+
+//! with the static keyword we change the linkage to internal
+static int add_internal(int x, int y) { //? this can't be forewarded
+    return x + y;
+}
+
