56752dffd219195f3cabec1b433f8a2c7234c3dc
Simon Gschnell <simongschnell@gmail.com>
2023-10-20 10:25:40 +0200

    goto in c++

---
+++ goto.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+
+    //! goto statements should be avoided because of the logical complexity they can create
+    //* goto statements can be easily be replaced with continue or break statements
+
+    double input {};
+
+loopLabel: // this is a statement label
+
+    std::cout << "Please enter a positive number:";
+    std::cin >> input;
+    if(input < 0.0) goto loopLabel; // this is unconditional jump
+    
+
+    std::cout << "The entered number is: " << input << std::endl;
+}
