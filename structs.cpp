77baa9b465a332e066dfe82d4627b2077c78b3a7
Simon Gschnell <simongschnell@gmail.com>
2023-11-04 17:40:02 +0100

    nested structs and unnamed returned structs

--- structs.cpp
+++ structs.cpp
@@ -7,11 +7,25 @@ struct Employee {
     double wage{};
 };
 
+//? we can also define structs that contain other structs as data members
+//* nested user-defined types
+struct PairEmployee {
+    std::string name{};
+    Employee coworker{};
+};
+
 struct ConstEmployee {
     int id{};   //* variables inside a struct are called data members
     int age{};  //* the curly brackets ensure that each data member is initialized with a start value
     double wage{};
 };
+
+//? when we return struct types we can omit the name and the type of the struct 
+//* because it is implicitly defined in the functions return type
+Employee return_normal_employee(){
+    // return Eployee {99, 22, 1020.50}; //! the same as bellow
+    return { 99, 22, 1020.50 }; //? returns a initialized employee struct object
+}
 
 int main(){
 
@@ -21,6 +35,9 @@ int main(){
     Employee peter { 3,22 }; //? all not declared values for data members will stay uninitialized
     const Employee hans { 4, 30, 10.3 }; //? we can also declare const structs which data members can not by modified
     constexpr Employee susie { 5, 10, 22.33 }; //? same for constexpr structs
+
+    PairEmployee pair { "Simon" , frank}; //* nested initialization using another Employee
+    PairEmployee pair { "Patrick" , {6, 41, 3000}}; //* nested aggregate initaliziation
 
     frank = {frank.id, 26, 2400 }; //* aggregate data member manipulation
     joe = frank; //? we can also structs of the same type to use as intialization value
