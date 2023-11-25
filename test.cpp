9da89378992d9d05dbd32cd504562f52e675e37c
Simon Gschnell <simongschnell@gmail.com>
2023-11-25 13:14:57 +0100

    shrink capacity to fit the lenght explicitly and expensive reallocations on resize

--- test.cpp
+++ test.cpp
@@ -25,11 +25,22 @@ int main(){
     //! the following line prints 6 because the size was doubled from 3 to 6 in memory 
     //! but only 5 are in active use because we called array.resize(5)
     std::cout << "the capacity of the array: " << array.capacity() << std::endl;
+    //? the subscript operator and the .at function will operate on the length of the array and not the capacity
+    //! which means index 5 which would be in the range of the containers capacity is considered as out of bounds
+    //std::cout << array.at(5) << std::endl; //! throws out_of_range error
 
     //* prints all elements
     for(const auto& element : array){
         std::cout << element << std::endl;
     }
+
+    array.resize(2);
+    std::cout << "size of array: " << array.size() << " capacity of array: " << array.capacity() << std::endl;
+    //? when shrinking a vector, the capacity will remain the same to avoid expensive reallocation
+    //! however if we also want to shrink the capacity explicitly, we have to call the member function shrink_to_fit()
+    array.shrink_to_fit();
+    std::cout << "AFTER calling shrink_to_fit - size of array: " << array.size() << " capacity of array: " << array.capacity() << std::endl;
+    
 
     //* procedure of resizing
     // - The std::vector acquires new memory with capacity for the desired number of elements. These elements are value-initialized.
