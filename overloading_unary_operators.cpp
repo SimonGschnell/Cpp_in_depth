a5603283be7db1a436afe3a922318b507d358fc6
Simon Gschnell <simongschnell@gmail.com>
2023-12-23 19:59:04 +0100

    how to use the unary ! operator to check wheter a user-defined object is true or false based on its data members

--- overloading_unary_operators.cpp
+++ overloading_unary_operators.cpp
@@ -6,16 +6,21 @@
 class Euro{
     public:
         Euro(int value): m_euros{value}{}
-        Euro operator- (){
+        Euro operator- () const{
             //* we overload the unary operator - as a member function, where the left most operand is the implicit this object
             //? implicitly converts int to Euro object because the constructor is not explicit
             return -m_euros;
         }
-        Euro operator+ (){
+        Euro operator+ () const {
             //? here we implement the overloaded operator+
             return +m_euros;
         }
         int get_amount() {return m_euros;}
+
+        //? we can use the unary operator! to check whether an object evaluates to true or false depending on the data member of the object 
+        bool operator! () const {
+            return m_euros > 0;
+        }
     private:
        int m_euros{};
 };
@@ -26,6 +31,12 @@ Euro eur1 {4};
 //? this will print -4 because the unary - operator with an Euro object will create and return a new Euro object with the same negative amount
 std::cout << (-eur1).get_amount() << std::endl;
 
+if(!eur1){
+    std::cout << "eur1 is 0 or negative" << std::endl;
+}
+else{
+    std::cout << "eur1 is greater than 0" << std::endl;
+}
 
 return 0;
 }
