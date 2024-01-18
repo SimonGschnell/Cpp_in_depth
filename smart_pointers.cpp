7f3b2103415f0830a63e1de464b9e2cd432d7379
Simon Gschnell <simongschnell@gmail.com>
2024-01-18 22:46:53 +0100

    smart pointers using move semantics, move semantics implemented in copy constructor and operator=

--- smart_pointers.cpp
+++ smart_pointers.cpp
@@ -1,28 +1,76 @@
 #include <iostream>
 
+class Resource{
+    public:
+    Resource(){
+        std::cout << "resource aquired" << std::endl;
+    }
+    ~Resource(){
+        std::cout << "resource deallocated" << std::endl;
+    }
+    void print(){
+        std::cout << "resource" << std::endl;
+    }
+};
+
 template <typename T>
 class AutoPtr{
     public:
         AutoPtr(T* ptr): m_ptr{ptr}{
-            std::cout << "resource allocated" << std::endl;
+            
         }
         ~AutoPtr(){
             delete m_ptr;
-            std::cout << "resource deallocated" << std::endl;
+            
         }
+
+        //! without defining a own copy constructor and overloading the operator= this class has a critical flaw
+        //! because the default copy constructor only does shallow copies and therefore multiple copies will point to the same resource and all try to delete the same
+        //? but pointers don't normally copy the resource they get assigned or get initialized with
+        //* therefore we can use move semantics, move semantics transfers ownership of the object rather than making a copy
+
+        //? implementing move semantics for the copy constructor
+        AutoPtr(AutoPtr& ptr){
+            m_ptr = ptr.m_ptr;
+            ptr.m_ptr = nullptr;
+        }
+        //? implements move semantics for the operator=
+        AutoPtr* operator= (AutoPtr& ptr){
+            if(this == &ptr){
+                return *this;
+            }
+            //? deleting a nullptr is ok, as it does nothing
+            delete m_ptr;
+            m_ptr = ptr.m_ptr;
+            ptr.m_ptr = nullptr;
+            
+            return *this;
+        }
+        
         T operator*()const{
             return *m_ptr;
         }
         T* operator->()const{
             return m_ptr;
         }
+        bool isNull()const {
+            if(m_ptr == nullptr)return true;
+            return false;
+        }
     private:
         T* m_ptr;
 };
 
 int main(){
-    AutoPtr<int> auto_ptr {new int{55}};
-    std::cout << *auto_ptr << std::endl;
+    AutoPtr auto_ptr {new Resource};
+    std::cout << std::boolalpha;
+    std::cout << auto_ptr.isNull() << std::endl;
+    
+    //? instead of shallow copying the auto_ptr it moves ownership of the resource
+    AutoPtr auto_ptr2 =auto_ptr;
+    std::cout << auto_ptr.isNull() << std::endl;
+    
+    
 
     return 0;
 }
