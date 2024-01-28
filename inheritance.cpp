cacdd1b68c8953543e95f1a5fb3caffc1a2d526a
Simon Gschnell <simongschnell@gmail.com>
2024-01-28 18:40:03 +0100

    different types of inheriting a parent class and their consequences

--- inheritance.cpp
+++ inheritance.cpp
@@ -10,6 +10,7 @@ class Person{
         std::string m_name{};
     //? C++ has a third access specifier that is specifically used for inheritance
     //~ the protected access specifier, gives access to member functions, friend functions and derived classes
+    //! favor private data members over protected data members
     protected: 
         int m_id{};
     public:
@@ -34,8 +35,11 @@ class Person{
 
 //? semicolon is used to inherit other classes
 //? this case is a public inheritance because the Person class was inherited with the public accessor
-//! when inheriting from a parent class, a child class cannot access the parent class private data members, getter and setter methods have to be used for this
-//! because private data members can only be accessed by member functions of the same class or friends
+//~ there are 3 types of inheriting another class
+//* - publicly inheriting a parent class -> keeps all the access specifiers of the parent data members the same, which still means that private data members of the parent are inaccessible
+//* - protected inheriting a parent class -> converts parents public data members to protected, protected stays protected and private stays inaccessible
+//* - privately inherting a parent class -> converts all the data members of the parent to private data members of the child class
+//! publicly and privately inheriting are the most used types, and privately inheriting is used if we don't want to expose the public interface of the parent class ( because for example the child class has no tight relation with the parent class)
 class BaseBallPlayer: public Person{
     private:
     int m_homeruns{};
@@ -47,6 +51,13 @@ class BaseBallPlayer: public Person{
         std::cout << "BaseBallPlayer Constructor" << std::endl;
         m_id = 55; //? protected data members from the parent class can be accessed directly by derrived classes
         //m_name = "Simon"; // private data members from the parent class can not be accessed by the derrived class //! compiler error: m_name is inaccessible
+    }
+
+    void printPersonInfo(){
+        //! when inheriting from a parent class, a child class cannot access the parent class private data members, getter and setter methods have to be used for this
+        //! because private data members can only be accessed by member functions of the same class or friends
+        // std::cout << "Person: "<< m_name << ", " << m_age << std::endl; //! compiler error m_name and m_age are inaccessible
+        std::cout << "Person: "<< getName() << ", " << getAge() << std::endl;
     }
     
     int getHomeruns(){
