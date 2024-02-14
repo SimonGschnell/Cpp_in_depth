b32e280460be79f3b41a133be7dffe589d9ca4f2
Simon Gschnell <simongschnell@gmail.com>
2024-02-14 22:19:00 +0100

    function catch blocks behaviors are different when used on constructors/destructors or functions

--- function_try_block.cpp
+++ function_try_block.cpp
@@ -18,13 +18,14 @@ class Derived: public Base{
 
 //? In order to catch exceptions that are raised when calling parent class constructors, we have to use function try blocks
 //* function try blocks can capture exceptions that are raised from the initialization list or inside the body of the function
-//! in contrast to normal catch blocks, function level catch blocks have to rethrow the new/original exception they cannot handle the exception themselves 
+//! ONLY FOR CONSTRUCTORS: function level catch blocks for constructors have to rethrow the new/original exception they cannot handle the exception themselves 
+//* function level catch blocks for destructors or normal functions can return a value and handle the exception themselves
 class Derived2: public Base{
     public:
         Derived2(int value) try : Base{value}{
         //* function try block captures everything inside the function including the intialization list    
         }catch(const int exception) {
-            //! function level catch blocks have to rethrow the exception
+            //! function level catch blocks have to rethrow the exception when used for constructor
             //? here we catch the failed creation of the parent Base object
             std::cout << "creation of Base failed" << std::endl;
 
@@ -32,6 +33,13 @@ class Derived2: public Base{
             throw;
         }
 };
+
+//? function try block on a normal function
+void throwException() try {
+    throw std::runtime_error("runtime error");
+}catch(const std::exception& exception){
+    std::cout << exception.what() << std::endl;
+}
 
 int main(){
 
@@ -48,6 +56,8 @@ int main(){
     catch(const int exception){
         std::cout << "main catch block" << std::endl;
     }
+
+    throwException();
 
     return 0;
 }
